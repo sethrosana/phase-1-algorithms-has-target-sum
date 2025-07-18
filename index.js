05e75699b7c1235127aaa103c1909428dc69ea19
sethmorara <seth.morara@student.moringaschool.com>
2025-07-18 13:01:44 +0300

    updates index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const showNumbers = new Set();
+
+  for (const num of array) {
+    const complement = target - num;
+    if (showNumbers.has(complement)) {
+      return true;
+    }
+    showNumbers.add(num);
+  }
+  return false;
 }
+console.log(hasTargetSum([7,5,8,9],12))
 
 /* 
   Write the Big O time complexity of your function here
+  O(1) (for each element) × n = O(n)
+
 */
 
 /* 
@@ -11,12 +23,21 @@ function hasTargetSum(array, target) {
 */
 
 /*
-  Add written explanation of your solution here
+  We start by creating an empty Set called showNumbers.
+  This Set will help us keep track of numbers we've already looked at as we go through the array.
+  We loop through each number (num) in the array
+  For each number, we calculate the complement, which is the number that would complete the pair to reach the target sum.
+  Example: If target is 12 and num is 5, then complement would be 7, because 5 + 7 = 10.
+  
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
